082aea4f3928adba2dfdff025d8ec3b8aa543056
DesignThinkerer <theophiledesmedt@gmail.com>
2024-06-12 22:05:14 +0200

    commit before update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,21 @@
 import { Routes } from '@angular/router';
+import { MainComponent } from './main/main.component';
 
 export const routes: Routes = [
-    
+    {
+        path: 'profile/:id',
+        redirectTo: (urlInfo) => {
+            console.log(urlInfo);
+            //must return a string or a UrlTree
+            return '/accueil'; 
+        },
+        pathMatch: 'full'
+    },
+    {
+        path: 'accueil',
+        component: MainComponent,
+        data: {
+            title: 'Bienvenue'
+        }
+    }
 ];
