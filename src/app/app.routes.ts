948351eabb7e167b1573e1ae7b7464b49083fbf1
DesignThinkerer <theophiledesmedt@gmail.com>
2024-06-12 23:48:09 +0200

    default route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,20 +2,5 @@ import { Routes } from '@angular/router';
 import { MainComponent } from './main/main.component';
 
 export const routes: Routes = [
-    {
-        path: 'profile/:id',
-        redirectTo: (urlInfo) => {
-            console.log(urlInfo);
-            
-            return '/'; 
-        },
-        pathMatch: 'full'
-    },
-    {
-        path: '',
-        component: MainComponent,
-        data: {
-            title: 'Bienvenue'
-        }
-    }
+    { path: '', component: MainComponent }
 ];
