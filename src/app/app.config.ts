734217627c1d239c47a26a2fa4421498a525274d
Théophile <34781168+DesignThinkerer@users.noreply.github.com>
2024-09-03 14:33:24 +0200

    add httpclient provider to the app config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,8 +4,9 @@ import { PreloadAllModules, RouterStateSnapshot, TitleStrategy, provideRouter, w
 import { routes } from './app.routes';
 import { Title, provideClientHydration } from '@angular/platform-browser';
 import { provideServiceWorker } from '@angular/service-worker';
+import { provideHttpClient } from '@angular/common/http';
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class TemplatePageTitleStrategy extends TitleStrategy {
   constructor(private readonly title: Title) {
     super();
@@ -22,18 +23,21 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideExperimentalZonelessChangeDetection(),
     provideRouter(
-      routes, 
+      routes,
       withComponentInputBinding(), //https://angular.dev/guide/routing/common-router-tasks#add-withcomponentinputbinding
       // withPreloading(PreloadAllModules) 
       // Note: si on a beaucoup de modules à charger, cela peut causer du lag. Dans ce cas, on peut utiliser le Quicklink preloading, voir: https://web.dev/articles/route-preloading-in-angular#using_the_preloadallmodules_strategy
 
       //https://angular.dev/guide/routing/common-router-tasks#preloading-lazy-loaded-modules
-    ), 
-    { provide: TitleStrategy, useClass: TemplatePageTitleStrategy }, // https://angular.dev/guide/routing/common-router-tasks#setting-the-page-title
+    ),
+    { provide: TitleStrategy, useClass: TemplatePageTitleStrategy },
+    // https://angular.dev/guide/routing/common-router-tasks#setting-the-page-title
 
     provideClientHydration(), provideServiceWorker('ngsw-worker.js', {
-            enabled: !isDevMode(),
-            registrationStrategy: 'registerWhenStable:30000'
-          })
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
+    //enable http requests with httpClient
+    provideHttpClient(),
   ]
 };
